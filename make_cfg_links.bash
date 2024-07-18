d5ae23b93d76ff892c46f79a5eae3ddd7604d337
njsoly <njsoly@syntj.com>
2024-07-18 03:18:48 -0500

    make_cfg_links.bash: New file to ensure that repo .CFGs are linked to home directory

---
+++ make_cfg_links.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##############################################################################################
+# make_cfg_links
+#
+# for the .cfg files I need for Klipper, make sure the repo files are linked to those in ~/.
+##############################################################################################
+
+ensure_link () {
+	file=$1
+	
+	# if the given inode ("file") has one name, it is not "linked".
+	if [ "$(stat -c %h -- "$file")" -gt 1 ]; then
+		printf "File \"${file}\" is not linked.\n"
+	else
+		printf "File \"${file}\" is already linked.\n"
+	fi
+}
+
+ensure_links () {
+	ensure_link printer.cfg
+	ensure_link example_leds.cfg
+}
+
+main () {
+	printf "${BASH_SOURCE}: making sure .cfg files are linked.\n"
+	ensure_links
+	
+	printf "${BASH_SOURCE}: finished.\n\n"
+}
+
+main
