0ab965da5754e6c3ef54311749a1c41429d604af
njsoly <njsoly@syntj.com>
2025-10-01 00:40:49 -0500

    zipped up klipper-out_240426, old build output folder

--- klipper-out_240426/README.md
+++
@@ -1,7 +0,0 @@
-# Klipper output directory 240426
-
-This is the output directory after calling `make menuconfig`, and possibly some other things, on 2024-04-26.
-
-`klipper_240501.bin` is the firmware file it created, and was loaded to the ender 5 s1 shortly afterward, possibly on May 1st.
-
-
--- klipper-out_240426/src/stm32/watchdog.d
+++
@@ -1,8 +0,0 @@
-out/src/stm32/watchdog.o: src/stm32/watchdog.c out/autoconf.h \
- src/stm32/internal.h lib/stm32f4/include/stm32f4xx.h \
- lib/stm32f4/include/stm32f401xc.h lib/cmsis-core/core_cm4.h \
- /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
- lib/cmsis-core/cmsis_version.h lib/cmsis-core/cmsis_compiler.h \
- lib/cmsis-core/cmsis_gcc.h lib/cmsis-core/mpu_armv7.h \
- lib/stm32f4/include/system_stm32f4xx.h src/sched.h src/ctr.h \
- src/compiler.h
